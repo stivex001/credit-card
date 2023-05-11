afcd017307dcbdcea0cc2a166333159b361cb96d
stivex001 <stephenadeyemo@gmail.com>
2023-05-11 15:53:04 +0100

    feat: added stylings to the form inputs

--- Frontend/src/components/Payment.jsx
+++ Frontend/src/components/Payment.jsx
@@ -4,11 +4,57 @@ import Cards from "react-credit-cards-2";
 import "react-credit-cards-2/dist/es/styles-compiled.css";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color: #82ccdd;
+  background-image: url("https://images.unsplash.com/photo-1525253086316-115d7aab269f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9");
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const FormWrapper = styled.form``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+  background: #E5E5E5;
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);
+`;
 
-const Input = styled.input``;
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 24px;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  margin: 12px 0;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+const Button = styled.button`
+  padding: 12px;
+  margin: 12px 0;
+  border: none;
+  border-radius: 4px;
+  background-color: #0070f3;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0055b8;
+  }
+`;
 
 const Payment = () => {
   const [formInputs, setFormInputs] = useState({
@@ -18,6 +64,7 @@ const Payment = () => {
     name: "",
     focus: "",
   });
+  const [error, setError] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -31,47 +78,50 @@ const Payment = () => {
 
   return (
     <Container>
-      <Cards
-        number={formInputs.number}
-        expiry={formInputs.expiry}
-        cvc={formInputs.cvc}
-        name={formInputs.name}
-        focused={formInputs.focus}
-      />
-      <FormWrapper>
-        <Input
-          type="number"
-          name="number"
-          placeholder="1234 1234 1234 1234"
-          value={formInputs.number}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
+      <Wrapper>
+        <Cards
+          number={formInputs.number}
+          expiry={formInputs.expiry}
+          cvc={formInputs.cvc}
+          name={formInputs.name}
+          focused={formInputs.focus}
         />
-        <Input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formInputs.name}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <Input
-          type="text"
-          name="expiry"
-          placeholder="MM/YY Expiry"
-          value={formInputs.expiry}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <Input
-          type="number"
-          name="cvc"
-          placeholder="CVC"
-          value={formInputs.cvc}
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-      </FormWrapper>
+        <FormWrapper>
+          <Input
+            type="number"
+            name="number"
+            placeholder="1234 1234 1234 1234"
+            value={formInputs.number}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+          />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formInputs.name}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+          />
+          <Input
+            type="text"
+            name="expiry"
+            placeholder="MM/YY Expiry"
+            value={formInputs.expiry}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+          />
+          <Input
+            type="number"
+            name="cvc"
+            placeholder="CVC"
+            value={formInputs.cvc}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+          />
+          <Button>Confirm Payment</Button>
+        </FormWrapper>
+      </Wrapper>
     </Container>
   );
 };
