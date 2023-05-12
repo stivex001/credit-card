b04caea8b3be11da6a74535f250a3b7ce6c467a2
stivex001 <stephenadeyemo@gmail.com>
2023-05-12 17:18:38 +0100

    feat: successfully connected to db

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,22 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+const app = express();
+dotenv.config();
+
+const DBConnect = () => {
+  mongoose
+    .connect(process.env.CONNECT_STRING)
+    .then(() => {
+      console.log("Succefully conneted to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 app.listen(8080, () => {
-    console.log(`listening on port 8080`)
-})
+    DBConnect()
+  console.log(`listening on port 8080 `);
+});
